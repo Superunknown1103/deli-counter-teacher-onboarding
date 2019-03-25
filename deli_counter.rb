388a31c792fbf4fd5984714bfe3b3c661408dcbb
Bruce Tsvilikhovsky <bruce.tsvilikhovsky@flatironschool.com>
2019-03-25 15:56:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+katz_deli = []
+
+def line(q)
+  if queue.empty?
+    puts "The line is currently empty."
+  else
+    result = "The line is currently: "
+    i = 0
+    while i < q.size
+    result += "#{i+1}. #{queue[i]}"
+    i+=1
+  end
+  puts result
+end
+end
